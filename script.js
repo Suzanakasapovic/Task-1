c1c7cece249fec0864ba030c99b97ee0c97d067d
Suzana Kasapovic <suzana.kasapovic@gmail.com>
2022-07-06 14:27:21 +0200

    Task 1 second version

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ function factorialize(num) {
     }
 }
 
+function factorializeForInput() {
+    var inputNumber = document.getElementById("factorialize-number").value;
+    var factorializedNum = factorialize(inputNumber);
+    document.getElementById("result").innerText=factorializedNum;
+}
+
 
 function checkPalindrome(string) {
     var len = string.length;
@@ -21,4 +27,17 @@ function checkPalindrome(string) {
         }
     }
     return 'It is palindrome'
+}
+
+
+function palindromeForInput() {
+    var word = document.getElementById("word").value;
+    var len = word.length;
+    for (var i = 0; i < len / 2; i++) {
+        if (word[i] !== word[len - 1 - i]) {
+            window.alert('It is not palindrome');
+        } else {
+    window.alert('It is palindrome');
+        }
+    }
 }
